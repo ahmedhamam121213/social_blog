cdecd7b44aa30eab2553a99e8fb406069e6e60c9
ahmedhamam121213 <hamamahmed64@gmail.com>
2019-07-09 03:32:32 +0200

    add login form

---
+++ head.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+
+<html lang="en">
+
+<head>
+  <meta charset="utf-8" />
+  <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" />
+  <title>
+    login
+  </title>
+  <meta content='width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0, shrink-to-fit=no' name='viewport' />
+  <!--     Fonts and icons     -->
+  <link href="https://fonts.googleapis.com/css?family=Montserrat:400,700,200" rel="stylesheet" />
+  <link href="https://maxcdn.bootstrapcdn.com/font-awesome/latest/css/font-awesome.min.css" rel="stylesheet">
+  <!-- CSS Files -->
+  <link href="assets/css/bootstrap.min.css" rel="stylesheet" />
+  <link href="assets/css/paper-kit.css?v=2.2.0" rel="stylesheet" />
+  <!-- CSS Just for demo purpose, don't include it in your project -->
+  <link href="assets/demo/demo.css" rel="stylesheet" />
+</head>
+
+<body class="register-page sidebar-collapse">
+  <!-- Navbar -->
+  <nav class="navbar navbar-expand-lg fixed-top navbar-transparent " color-on-scroll="300">
+    <div class="container">
+      <div class="navbar-translate">
+        <a class="navbar-brand" href="#" rel="tooltip" title="Coded by Creative Tim" data-placement="bottom" target="_blank">
+          Blog System
+        </a>
+        <button class="navbar-toggler navbar-toggler" type="button" data-toggle="collapse" data-target="#navigation" aria-controls="navigation-index" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-bar bar1"></span>
+          <span class="navbar-toggler-bar bar2"></span>
+          <span class="navbar-toggler-bar bar3"></span>
+        </button>
+      </div>
+      <div class="collapse navbar-collapse justify-content-end" id="navigation">
+        <ul class="navbar-nav">
+          <!-- <li class="nav-item">
+            <a href="#" target="_blank" class="nav-link"> Documentation</a>
+          </li> -->
+        </ul>
+      </div>
+    </div>
+  </nav>
+  <!-- End Navbar -->
---
+++ register-page.php
@@ -0,0 +1,24 @@
+
+  <?php require_once("head.php"); ?>
+  <div class="page-header" style="background-image: url('https://images.pexels.com/photos/1559041/pexels-photo-1559041.jpeg?cs=srgb&dl=background-beverage-blank-1559041.jpg&fm=jpg');background-size: cover">
+    <div class="filter"></div>
+    <div class="container">
+      <div class="row">
+        <div class="col-lg-4 ml-auto mr-auto">
+          <div class="card card-register">
+            <h3 class="title mx-auto">Welcome</h3>
+            <form class="register-form">
+              <label>Username</label>
+              <input type="text" class="form-control" placeholder="Username">
+              <label>Password</label>
+              <input type="password" class="form-control" placeholder="Password">
+              <button type="submit" name="submit" class="btn btn-danger btn-block btn-round">Login</button>
+            </form>
+           
+          </div>
+        </div>
+      </div>
+    </div>
+
+  </div>
+<?php require_once("footer.php"); ?>
